a45f0ccc5e7b3f1503825d29522119b4d62acfdb
HsiangHo <ho_cheung@qq.com>
2020-06-18 14:24:27 +0800

    new methods

--- WWDC Pass Generator/WWDC Pass Generator/ColorExtension.swift
+++ WWDC Pass Generator/WWDC Pass Generator/ColorExtension.swift
@@ -33,4 +33,23 @@ extension Color {
             opacity: Double(a) / 255
         )
     }
+
+    var nsColor: NSColor {
+        return NSColor(red: rgba.r, green: rgba.g, blue: rgba.b, alpha: rgba.a)
+    }
+
+    var rgba: (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+        let scanner = Scanner(string: self.description.trimmingCharacters(in: CharacterSet.alphanumerics.inverted))
+        var hexNumber: UInt64 = 0
+        var r: CGFloat = 0.0, g: CGFloat = 0.0, b: CGFloat = 0.0, a: CGFloat = 0.0
+
+        let result = scanner.scanHexInt64(&hexNumber)
+        if result {
+            r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+            g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+            b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+            a = CGFloat(hexNumber & 0x000000ff) / 255
+        }
+        return (r, g, b, a)
+    }
 }
